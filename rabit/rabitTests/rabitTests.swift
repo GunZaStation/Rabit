70a24ed40bee078b6ae8fd79575f755286aa55c5
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-02 12:52:23 +0900

    build: 프로젝트 생성

---
+++ rabit/rabitTests/rabitTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import rabit
+
+class rabitTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+
+    func testPerformanceExample() throws {
+        
+        self.measure {}
+    }
+}
