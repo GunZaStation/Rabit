4915389e9c906135225ea75c482df7117b99f8f2
Sol <hskim6521@naver.com>
2022-08-03 18:29:23 +0900

    feat: [#3] AlbumViewController의 collectionview에서 사용할 HeaderView 제작

---
+++ rabit/rabit/Album/AlbumHeaderView.swift
@@ -0,0 +1,46 @@
+import UIKit
+import SnapKit
+
+final class AlbumHeaderView: UICollectionReusableView {
+    static let identifier = String(describing: AlbumHeaderView.self)
+
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.clipsToBounds = true
+        label.layer.cornerCurve = .continuous
+        label.layer.cornerRadius = 10
+        label.backgroundColor = .blue
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        setupViews()
+    }
+
+    func configure(with text: String?) {
+        dateLabel.text = text
+    }
+}
+
+private extension AlbumHeaderView {
+    // MARK: - Setup UI
+    func setupViews() {
+        addSubview(dateLabel)
+
+        dateLabel.snp.makeConstraints { make in
+            make.top.bottom.equalTo(self)
+            make.leading.equalTo(self).offset(10)
+            make.trailing.equalTo(self).inset(10)
+        }
+    }
+}
