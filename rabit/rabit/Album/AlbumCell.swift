9d1d68639659614dd7651b151692b721f84ac868
Sol <hskim6521@naver.com>
2022-08-03 17:02:09 +0900

    feat: [#3] AlbumViewController에서 collectionview Cell로 사용할 AlbumCell 제작

---
+++ rabit/rabit/Album/AlbumCell.swift
@@ -0,0 +1,36 @@
+import UIKit
+import SnapKit
+
+final class AlbumCell: UICollectionViewCell {
+    static let identifier = String(describing: AlbumCell.self)
+
+    private let thumbNailViewController = ThumbnailPictureViewController()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        setupViews()
+    }
+
+    func configure(with images: [UIImage?]) {
+        thumbNailViewController.update(with: images)
+    }
+}
+
+private extension AlbumCell {
+    // MARK: - Setup UI
+    func setupViews() {
+        clipsToBounds = true
+        addSubview(thumbNailViewController.view)
+
+        thumbNailViewController.view.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+    }
+}
