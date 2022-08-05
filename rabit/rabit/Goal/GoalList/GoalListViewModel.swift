3fbc39b08e633968fb36615af43b9be97fa6a012
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-06 02:17:58 +0900

    feat: [#6] Goal 구조체에 SectionModelType 프로토콜 채택 추가

--- rabit/rabit/Goal/GoalList/GoalListViewModel.swift
+++ rabit/rabit/Goal/GoalList/GoalListViewModel.swift
@@ -39,7 +39,7 @@ extension GoalListViewModel {
             GoalDetail(title: "스트레칭", subtitle: "스트레칭 하기", progress: 20, target: 100),
             GoalDetail(title: "보조 운동", subtitle: "팔과 복근", progress: 40, target: 100),
         ]
-        sections[0].details = models
+        sections[0].items = models
     }
     
     private func category2() {
@@ -47,7 +47,7 @@ extension GoalListViewModel {
             GoalDetail(title: "CS 공부", subtitle: "운영체제, 네트워크 스터디", progress: 10, target: 100),
             GoalDetail(title: "Swift 공부", subtitle: "WWDC 발표자료 정리", progress: 20, target: 100)
         ]
-        sections[1].details = models
+        sections[1].items = models
     }
     
     private func category3() {
@@ -55,13 +55,13 @@ extension GoalListViewModel {
             GoalDetail(title: "영양제", subtitle: "비타민 먹기", progress: 40, target: 80),
             GoalDetail(title: "수면 패턴", subtitle: "일찍 자기", progress: 60, target: 120)
         ]
-        sections[2].details = models
+        sections[2].items = models
     }
     
     private func category4() {
         let models = [
             GoalDetail(title: "만화책 읽기", subtitle: "짱구는 못말려 완독", progress: 50, target: 200)
         ]
-        sections[3].details = models
+        sections[3].items = models
     }
 }
