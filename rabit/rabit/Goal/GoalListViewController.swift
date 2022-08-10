edb18fbb949b5c82479ba8a871dd4f0f373e1b9b
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-10 17:48:42 +0900

    feat: [#6] 목표 컬렉션 뷰에 헤더 뷰 적용

--- rabit/rabit/Goal/GoalListViewController.swift
+++ rabit/rabit/Goal/GoalListViewController.swift
@@ -7,34 +7,59 @@ import RxSwift
 final class GoalListViewController: UIViewController {
     
     typealias DataSource = RxCollectionViewSectionedReloadDataSource
-    private let goalListDataSource = DataSource<Goal> { _, collectionView, indexPath, goalDetail in
-        
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GoalListCollectionViewCell.identifier, for: indexPath) as? GoalListCollectionViewCell else { return UICollectionViewCell() }
-        
-        
-        cell.configure(title: goalDetail.title, subtitle: goalDetail.subtitle, progress: goalDetail.progress, target: goalDetail.target)
-        
-        return cell
-    }
+    private let goalListDataSource = DataSource<Goal>(
+        configureCell: { dataSource, collectionView, indexPath, goalDetail in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GoalListCollectionViewCell.identifier,
+                                                                for: indexPath) as? GoalListCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            cell.configure(title: goalDetail.title, subtitle: goalDetail.subtitle, progress: goalDetail.progress, target: goalDetail.target)
+            
+            return cell
+        },
+        configureSupplementaryView: {(dataSource, collectionView, kind, indexPath) -> UICollectionReusableView in
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                               withReuseIdentifier: GoalListCollectionViewHeader.identifier,
+                                                                               for: indexPath) as? GoalListCollectionViewHeader else {
+                return UICollectionReusableView()
+            }
+            
+            let goal = dataSource.sectionModels[indexPath.section]
+            header.configure(title: goal.category)
+            
+            return header
+        }
+    )
     
     private lazy var goalListCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = .init(top: .zero, left: .zero, bottom: 15, right: .zero)
         layout.itemSize = CGSize(width: view.frame.width*0.85, height: view.frame.height*0.15)
-        
+        layout.headerReferenceSize = CGSize(width: view.frame.width*0.85, height: view.frame.height*0.07)
+
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
         collectionView.register(
             GoalListCollectionViewCell.self,
             forCellWithReuseIdentifier: GoalListCollectionViewCell.identifier
         )
+        
+        collectionView.register(
+            GoalListCollectionViewHeader.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: GoalListCollectionViewHeader.identifier
+        )
+        
         collectionView.showsVerticalScrollIndicator = false
+        
         return collectionView
     }()
     
     private var viewModel: GoalListViewModel?
     private let disposeBag = DisposeBag()
-
+    
     convenience init(viewModel: GoalListViewModel = GoalListViewModel()) {
         
         self.init()
@@ -51,7 +76,7 @@ final class GoalListViewController: UIViewController {
     }
     
     private func bind() {
-  
+        
         viewModel?.categories
             .bind(to: goalListCollectionView.rx.items(dataSource: goalListDataSource))
             .disposed(by: disposeBag)
