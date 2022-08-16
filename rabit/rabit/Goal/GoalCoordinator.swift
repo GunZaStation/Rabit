13fd177cfb07e70a12cea1ca6d8245c02ee03a38
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-16 17:27:33 +0900

    refactor: [#21] GoalCoordinator에 PublishRelay 타입 속성 추가

--- rabit/rabit/Goal/GoalCoordinator.swift
+++ rabit/rabit/Goal/GoalCoordinator.swift
@@ -1,42 +1,56 @@
 import UIKit
-
-protocol GoalNavigation: AnyObject {
-    
-    func showCategoryAddView()
-}
+import RxSwift
+import RxRelay
 
 final class GoalCoordinator: Coordinator {
 
     weak var parentCoordiantor: Coordinator?
     var children: [Coordinator] = []
     var navigationController: UINavigationController
+    
+    let showCategoryAddView = PublishRelay<Void>()
+    let closeCategoryAddView = PublishRelay<Void>()
+    
+    private let disposeBag = DisposeBag()
 
     init() {
         self.navigationController = UINavigationController()
         self.navigationController.view.backgroundColor = .systemBackground
     }
+    
+    private func bind() {
+        
+        showCategoryAddView
+            .bind(onNext: presentCategoryAddViewControllr)
+            .disposed(by: disposeBag)
+        
+        closeCategoryAddView
+            .bind(onNext: dismissCategoryAddViewController)
+            .disposed(by: disposeBag)
+    }
 
     func start() {
         parentCoordiantor?.navigationController.setNavigationBarHidden(true, animated: false)
         pushGoalListViewController()
+        bind()
     }
     
     private func pushGoalListViewController() {
-        let viewModel = GoalListViewModel()
-        viewModel.navigation = self
+        let viewModel = GoalListViewModel(coordinator: self)
         let viewController = GoalListViewController(viewModel: viewModel)
         navigationController.pushViewController(viewController, animated: true)
     }
-}
-
-extension GoalCoordinator: GoalNavigation {
     
-    func showCategoryAddView() {
-        
-        let viewModel = CategoryAddViewModel()
-        viewModel.navigation = self
+    private func presentCategoryAddViewControllr() {
+
+        let viewModel = CategoryAddViewModel(coordinator: self)
         let viewController = CategoryAddViewController(viewModel: viewModel)
         viewController.modalPresentationStyle = .overCurrentContext
         navigationController.present(viewController, animated: false)
+    }
+    
+    private func dismissCategoryAddViewController() {
+        
+        navigationController.presentedViewController?.dismiss(animated: false)
     }
 }
