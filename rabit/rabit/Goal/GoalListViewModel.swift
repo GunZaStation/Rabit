e0bc9e671bcc0c083a9f38619e3dad3c2f190046
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-16 17:28:01 +0900

    refactor: [#21] GoalListViewModel과 GoalCoordinator의 속성 간 바인딩 적용

--- rabit/rabit/Goal/GoalListViewModel.swift
+++ rabit/rabit/Goal/GoalListViewModel.swift
@@ -19,24 +19,20 @@ final class GoalListViewModel: GoalListViewModelInput, GoalListViewModelOutput {
     let categoryAddButtonTouched = PublishRelay<Void>()
     let goalList = PublishRelay<[Goal]>()
     
-    weak var navigation: GoalNavigation?
-    
     private let repository: GoalListRepository
     private let disposeBag = DisposeBag()
     
-    init(repository: GoalListRepository = GoalListRepository()) {
+    init(repository: GoalListRepository = GoalListRepository(),
+         coordinator: GoalCoordinator) {
         self.repository = repository
         
-        bind()
+        bind(coordinator: coordinator)
     }
     
-    private func bind() {
+    private func bind(coordinator: GoalCoordinator) {
         
         categoryAddButtonTouched
-            .withUnretained(self)
-            .bind(onNext: { viewModel, _ in
-                viewModel.navigation?.showCategoryAddView()
-            })
+            .bind(to: coordinator.showCategoryAddView)
             .disposed(by: disposeBag)
         
         requestGoalList
