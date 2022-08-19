3b5aa765fd2c184cfa4dbbbfd670f8e757fa50d8
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-19 17:09:50 +0900

    feat: [#13] GoalAddViewController, InsertField 구현 및 추가

---
+++ rabit/rabit/Goal/GoalAdd/Views/InsertField.swift
@@ -0,0 +1,64 @@
+import UIKit
+import SnapKit
+
+final class InsertField: UIView {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let textField: UITextField = {
+        let textField = UITextField()
+        textField.clipsToBounds = true
+        textField.layer.borderColor = UIColor.systemGray4.cgColor
+        textField.layer.borderWidth = 1.0
+        textField.layer.cornerRadius = 9
+        return textField
+    }()
+    
+    var title: String = "" {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    
+    var placeholder: String = "" {
+        didSet {
+            textField.placeholder = " \(placeholder)"
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setAttributes()
+        addViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setAttributes() {
+//        clipsToBounds = true
+//        layer.borderColor = UIColor.systemGray4.cgColor
+//        layer.borderWidth = 1.0
+//        layer.cornerRadius = 10
+    }
+    
+    private func addViews() {
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.leading.centerY.equalToSuperview()
+            $0.width.equalToSuperview().multipliedBy(0.2)
+        }
+        
+        addSubview(textField)
+        textField.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.trailing).offset(10)
+            $0.height.centerY.trailing.equalToSuperview()
+        }
+    }
+}
