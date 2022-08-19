0d490fa028f9c5e6078d3906e19f5f7616dd0d3a
Sol <hskim6521@naver.com>
2022-08-19 14:42:32 +0900

    fix: [#24] alpha 값을 고려하지 않아 생겼던 이슈 해결

--- rabit/rabit/Utilities/UIColor + Initializer.swift
+++ rabit/rabit/Utilities/UIColor + Initializer.swift
@@ -1,41 +1,37 @@
 import UIKit
 
 extension UIColor {
-    convenience init?(hex: String) {
-        let r, g, b, a: CGFloat
-
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-            }
+    convenience init?(hexRGBA: String?) {
+        guard let rgba = hexRGBA,
+              let val = Int(rgba.replacingOccurrences(of: "#", with: ""), radix: 16) else {
+            return nil
         }
-        return nil
+
+        self.init(
+            red: CGFloat((val >> 24) & 0xff) / 255.0,
+            green: CGFloat((val >> 16) & 0xff) / 255.0,
+            blue: CGFloat((val >> 8) & 0xff) / 255.0,
+            alpha: CGFloat(val & 0xff) / 255.0
+        )
     }
 
+    convenience init?(hexRGB: String?) {
+        guard let rgb = hexRGB else {
+            return nil
+        }
+        self.init(hexRGBA: rgb + "ff") // Add alpha = 1.0
+    }
+    
     func toHexString() -> String {
         var r: CGFloat = 0
         var g: CGFloat = 0
         var b: CGFloat = 0
         var a: CGFloat = 0
-
+        
         getRed(&r, green: &g, blue: &b, alpha: &a)
-
-        let rgb: Int = (Int)(r * 255) << 16 | (Int)(g*255) << 8 | (Int)(b*255) << 0
-
-        return NSString(format: "#%06x", rgb) as String
+        
+        let rgb: Int = (Int)(r * 255) << 24 | (Int)(g*255) << 16 | (Int)(b*255) << 8 | (Int)(a*255) << 0
+        
+        return NSString(format: "#%08x", rgb) as String
     }
 }
