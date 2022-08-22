b9431fdfbce64c59840dd8143c5e81d1feab37d3
Sol <hskim6521@naver.com>
2022-08-22 14:28:28 +0900

    feat: [#30] Tab Bar Icon 뒤에 동그란 색 배경을 위한 UIImage extension 추가

--- rabit/rabit/Utilities/UIImage + Utils.swift
+++ rabit/rabit/Utilities/UIImage + Utils.swift
@@ -6,4 +6,14 @@ extension UIImage {
             draw(in: CGRect(origin: .zero, size: size))
         }
     }
+    
+    static func imageWithColor(color: UIColor?, size: CGSize) -> UIImage {
+        let rect: CGRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        (color ?? .white).setFill()
+        UIBezierPath(ovalIn: rect).fill()
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
 }
