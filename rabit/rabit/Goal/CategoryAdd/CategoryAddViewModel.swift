cd1bf41a1927e7751049d43b18b1789fbe4ab0a8
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-26 01:26:06 +0900

    feat: [#36] CategoryAddViewModel에 버튼 비활성화 여부, 입력값의 중복 여부 및 공백 여부, 카테고리 추가 성공 여부에 대한 옵저버 추가

--- rabit/rabit/Goal/CategoryAdd/CategoryAddViewModel.swift
+++ rabit/rabit/Goal/CategoryAdd/CategoryAddViewModel.swift
@@ -6,12 +6,15 @@ protocol CategoryAddViewModelInput {
     
     var categoryTitleInput: PublishRelay<String> { get }
     var saveButtonTouched: PublishRelay<Void> { get }
+    var saveButtonDisabled: BehaviorRelay<Bool> { get }
     var closeButtonTouched: PublishRelay<Void> { get }
+    var categoryAddResult: PublishRelay<Bool> { get }
 }
 
 protocol CategoryAddViewModelOutput {
     
-    var categoryTitleOutput: PublishRelay<String> { get }
+    var titleInputDuplicated: PublishRelay<Bool> { get }
+    var titleInputEmpty: PublishRelay<Bool> { get }
 }
 
 final class CategoryAddViewModel: CategoryAddViewModelInput, CategoryAddViewModelOutput {
@@ -20,10 +23,19 @@ final class CategoryAddViewModel: CategoryAddViewModelInput, CategoryAddViewMode
     let closeButtonTouched = PublishRelay<Void>()
     let categoryTitleInput = PublishRelay<String>()
     let categoryTitleOutput = PublishRelay<String>()
+    let categoryAddResult = PublishRelay<Bool>()
+    
+    let saveButtonDisabled = BehaviorRelay<Bool>(value: true)
+    let titleInputDuplicated = PublishRelay<Bool>()
+    let titleInputEmpty = PublishRelay<Bool>()
     
     private let disposeBag = DisposeBag()
+    private let repository: CategoryAddRepository
     
-    init(navigation: GoalNavigation) {
+    init(navigation: GoalNavigation,
+         repository: CategoryAddRepository = CategoryAddRepository()) {
+        self.repository = repository
+        
         bind(to: navigation)
     }
     
@@ -34,17 +46,39 @@ private extension CategoryAddViewModel {
     func bind(to navigation: GoalNavigation) {
         
         categoryTitleInput
-            .bind(to: categoryTitleOutput)
+            .map { $0.isEmpty }
+            .bind(to: titleInputEmpty)
             .disposed(by: disposeBag)
         
-        categoryTitleOutput
-            .bind(onNext: {
-                print($0)
-            })
+        categoryTitleInput
+            .withUnretained(self)
+            .map { $0.repository.checkTitleDuplicated(input: $1) }
+            .bind(to: titleInputDuplicated)
+            .disposed(by: disposeBag)
+        
+        Observable
+            .combineLatest(titleInputEmpty.asObservable(), titleInputDuplicated.asObservable())
+            .map { $0 || $1 }
+            .bind(to: saveButtonDisabled)
             .disposed(by: disposeBag)
         
         closeButtonTouched
             .bind(to: navigation.closeCategoryAddView)
+            .disposed(by: disposeBag)
+        
+        saveButtonTouched
+            .withLatestFrom(categoryTitleInput)
+            .withUnretained(self)
+            .flatMapLatest { viewModel, title in
+                viewModel.repository.addCategory(goal: Goal(category: title))
+            }
+            .bind(to: categoryAddResult)
+            .disposed(by: disposeBag)
+        
+        categoryAddResult
+            .bind(onNext: { isSuccess in
+                if isSuccess { navigation.closeCategoryAddView.accept(()) }
+            })
             .disposed(by: disposeBag)
     }
 }
