78879e292de15482d4ee46f036a48eeda88fdc5b
Sol <hskim6521@naver.com>
2022-08-25 15:42:12 +0900

    refactor: [#36]  AlbumRepository에서 하드코딩으로 데이터 추가하던 부분 모두 제거하고 Realm에서 데이터 가져오도록 수정

--- rabit/rabit/Album/Repository/AlbumRepository.swift
+++ rabit/rabit/Album/Repository/AlbumRepository.swift
@@ -1,6 +1,5 @@
 import Foundation
 import RxSwift
-import UIKit // MockData의 UIImage를 위해 임시적으로 import
 
 protocol AlbumRepositoryProtocol {
     func fetchAlbumData() -> Observable<[Album]>
@@ -8,32 +7,20 @@ protocol AlbumRepositoryProtocol {
 
 final class AlbumRepository: AlbumRepositoryProtocol {
     func fetchAlbumData() -> Observable<[Album]> {
-        let backgroundQueue = DispatchQueue(label: "albumRepository.queue")
 
         return Observable.create { observer in
-            backgroundQueue.async {
-                // 통신 중인 것처럼 보이게 하기 위해 2초 딜레이
-                Thread.sleep(forTimeInterval: 2)
 
-                // 통신 여부와 상관없이 MockData 반환
-                let fetchedMockData = [
-                    Album(date: Date(), items: [
-                        Photo(imageData: UIImage(systemName: "pencil")!.pngData()!, date: Date(), color: "#FFFFFF"),
-                        Photo(imageData: UIImage(systemName: "pencil")!.pngData()!, date: Date(), color: "#FFFFFF"),
-                        Photo(imageData: UIImage(systemName: "pencil")!.pngData()!, date: Date(), color: "#FFFFFF")
-                    ]),
-                    Album(date: Date(), items: [
-                        Photo(imageData: UIImage(systemName: "pencil")!.pngData()!, date: Date(), color: "#FFFFFF"),
-                        Photo(imageData: UIImage(systemName: "pencil")!.pngData()!, date: Date(), color: "#FFFFFF"),
-                        Photo(imageData: UIImage(systemName: "pencil")!.pngData()!, date: Date(), color: "#FFFFFF")
-                    ]),
-                    Album(date: Date(), items: [
-                        Photo(imageData: UIImage(systemName: "pencil")!.pngData()!, date: Date(), color: "#FFFFFF"),
-                    ])
-                ]
-               
-                observer.onNext(fetchedMockData)
+            let realmManager = RealmRepository.shared
+
+            let fetchedData = realmManager.read(entity: PhotoEntity.self)
+
+            var albumData = Album(date: fetchedData.first?.date ?? Date(), items: [])
+
+            fetchedData.forEach {
+                albumData.items.append(Photo(entity: $0))
             }
+
+            observer.onNext([albumData])
 
             return Disposables.create()
         }
