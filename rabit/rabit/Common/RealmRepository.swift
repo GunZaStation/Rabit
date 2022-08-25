19a902bce921f784f3fe884a19c7d2983649cc0a
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-26 01:24:09 +0900

    refactor: [#36] RealmRepository.read에 query 파라미터 추가
    - 사용 안할 수도 있기 때문에 디폴트 파라미터로 빈 문자열을 정의

--- rabit/rabit/Common/RealmRepository.swift
+++ rabit/rabit/Common/RealmRepository.swift
@@ -10,8 +10,12 @@ final class RealmRepository {
         self.realm = try! Realm()
     }
     
-    func read<T: Object>(entity: T.Type) -> Results<T> {
-        return realm.objects(entity)
+    func read<T: Object>(entity: T.Type, filter query: String = "") -> Results<T> {
+        if query.isEmpty {
+            return realm.objects(entity)
+        } else {
+            return realm.objects(entity).filter(query)
+        }
     }
  
     func write(entity: Object) {
