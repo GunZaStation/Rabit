78ce2e5048f7c37f530fcf7d46e60b560a34718a
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-08-28 02:24:32 +0900

    feat: [#31] BottomSheet에 애니메이션 처리를 위한 move 메소드 추가

--- rabit/rabit/Common/Views/BottomSheet.swift
+++ rabit/rabit/Common/Views/BottomSheet.swift
@@ -20,6 +20,7 @@ final class BottomSheet: UIView {
     }
     
     private func setupViews() {
+        
         addSubview(topBar)
         topBar.snp.makeConstraints {
             $0.top.equalToSuperview().offset(10)
@@ -27,5 +28,26 @@ final class BottomSheet: UIView {
             $0.centerX.equalToSuperview()
             $0.height.equalTo(8)
         }
+    }
+}
+
+extension BottomSheet {
+
+    func move(upTo topConstraint: CGFloat,
+              duration: CGFloat,
+              animation: @escaping () -> Void,
+              completion: @escaping (Bool) -> Void = { _ in }) {
+        
+        self.snp.remakeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(topConstraint)
+        }
+        
+        UIView.animate(
+            withDuration: duration,
+            delay: 0,
+            animations: animation,
+            completion: completion
+        )
     }
 }
