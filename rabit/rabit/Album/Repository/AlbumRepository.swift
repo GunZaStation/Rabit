5b70512042d86d0e2b6fe3c4218628896a304a22
Sol <hskim6521@naver.com>
2022-09-04 21:07:20 +0900

    refactor: [#42] AlbumRepository의 fetchAlbumData() 메소드 반환타입 변경

--- rabit/rabit/Album/Repository/AlbumRepository.swift
+++ rabit/rabit/Album/Repository/AlbumRepository.swift
@@ -2,7 +2,7 @@ import Foundation
 import RxSwift
 
 protocol AlbumRepositoryProtocol {
-    func fetchAlbumData() -> Observable<[Album]>
+    func fetchAlbumData() -> Single<[Album]>
     func updateAlbumData(_ data: Photo) -> Single<Bool>
 }
 
@@ -10,32 +10,11 @@ final class AlbumRepository: AlbumRepositoryProtocol {
 
     private let realmManager = RealmManager.shared
 
-    func fetchAlbumData() -> Observable<[Album]> {
+    func fetchAlbumData() -> Single<[Album]> {
+        let fetchedAlbumData = getLatestAlbum()
 
-        return Observable.create { [weak self] observer in
-
-            guard let self = self else { return Disposables.create() }
-
-            let fetchedGoalDetailData = self.realmManager.read(entity: CategoryEntity.self)
-
-            var albumData = [Album]()
-
-            fetchedGoalDetailData.forEach {
-                let categoryTitle = $0.title
-
-                let fetchedPhotoData = self.realmManager
-                    .read(
-                        entity: PhotoEntity.self,
-                        filter: "categoryTitle == '\(categoryTitle)'"
-                    )
-                    .map { Photo(entity: $0) }
-
-                if !fetchedPhotoData.isEmpty {
-                    albumData.append(Album(categoryTitle: categoryTitle, items: fetchedPhotoData))
-                }
-            }
-
-            observer.onNext(albumData)
+        return .create { single in
+            single(.success(fetchedAlbumData))
 
             return Disposables.create()
         }
@@ -55,5 +34,29 @@ final class AlbumRepository: AlbumRepositoryProtocol {
 
             return Disposables.create()
         }
+    }
+}
+
+private extension AlbumRepository {
+    func getLatestAlbum() -> [Album] {
+        let fetchedCategoryData = realmManager.read(entity: CategoryEntity.self)
+
+        var albumData = [Album]()
+
+        fetchedCategoryData.forEach {
+            let categoryTitle = $0.title
+
+            let fetchedPhotoData = realmManager.read(entity: PhotoEntity.self, filter: "categoryTitle == '\(categoryTitle)'")
+                .map(Photo.init)
+
+            if !fetchedPhotoData.isEmpty {
+                albumData.append(Album(
+                    categoryTitle: categoryTitle,
+                    items: fetchedPhotoData
+                ))
+            }
+        }
+
+        return albumData
     }
 }
