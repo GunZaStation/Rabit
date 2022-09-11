279910382e0a441dcadcb321cd1ebdedde77f9b0
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-09-11 19:32:24 +0900

    feat: [#48] Goal의 CertifiableTime 속성에 대응되도록 GoalEntity에 속성 추가 정의

--- rabit/rabit/Goal/Models/GoalEntity.swift
+++ rabit/rabit/Goal/Models/GoalEntity.swift
@@ -7,8 +7,17 @@ final class GoalEntity: Object {
     @Persisted var progress: Int = 0
     @Persisted var target: Int = 0
     @Persisted var category: String = ""
+    @Persisted var startCertTime: Int = 0
+    @Persisted var endCertTime: Int = 0
+    @Persisted var certDays: List<Int> = List()
     
-    convenience init(title: String, subtitle: String, progress: Int, target: Int, category: String) {
+    convenience init(
+        title: String,
+        subtitle: String,
+        progress: Int,
+        target: Int,
+        category: String,
+        certTime: CertifiableTime) {
         self.init()
         
         self.title = title
@@ -16,5 +25,19 @@ final class GoalEntity: Object {
         self.progress = progress
         self.target = target
         self.category = category
+        self.startCertTime = certTime.start.toSeconds()
+        self.endCertTime = certTime.end.toSeconds()
+        self.certDays.append(objectsIn: certTime.days.toIntArray())
+    }
+}
+
+extension Days {
+    
+    init(_ list: List<Int>) {
+        self.set = Set(list.compactMap { Day(rawValue: $0) })
+    }
+    
+    func toIntArray() -> [Int] {
+        return Array(self.set).map { $0.rawValue }
     }
 }
