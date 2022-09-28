f09f1bd667f6144ca1952a2e261de23f6f7e514c
Sol <hskim6521@naver.com>
2022-09-28 22:51:05 +0900

    chore: [#50] 불필요하게 제거한 코드 다시 추가

--- rabit/rabit/Album/AlbumViewController.swift
+++ rabit/rabit/Album/AlbumViewController.swift
@@ -148,6 +148,7 @@ private extension AlbumViewController {
         environment: NSCollectionLayoutEnvironment
     ) {
         items.forEach { item in
+            guard item.representedElementKind != UICollectionView.elementKindSectionHeader else { return }
             let cellWidth = environment.container.contentSize.width
             let distanceFromCenter = abs((item.frame.midX - offset.x) - cellWidth / 2.0)
 
