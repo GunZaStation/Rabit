13a0adb36bcbea397e0d7ed6b281ae60252db1b3
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-10-18 03:13:20 +0900

    feat: [#74] CertPhotoCameraViewModel 구현

---
+++ rabit/rabit/Goal/CertPhotoCamera/CertPhotoCameraViewModel.swift
@@ -0,0 +1,40 @@
+import Foundation
+import RxSwift
+import RxRelay
+
+protocol CertPhotoCameraViewInput {
+    
+    var certPhotoDataInput: PublishRelay<Data> { get }
+    var nextButtonTouched: PublishRelay<Void> { get }
+}
+protocol CertPhotoCameraViewOutput {
+    
+    var previewPhotoData: PublishRelay<Data> { get }
+}
+
+final class CertPhotoCameraViewModel: CertPhotoCameraViewInput, CertPhotoCameraViewOutput {
+    
+    let certPhotoDataInput = PublishRelay<Data>()
+    let nextButtonTouched = PublishRelay<Void>()
+    let previewPhotoData = PublishRelay<Data>()
+    
+    private let disposeBag = DisposeBag()
+    
+    init(navigation: GoalNavigation, goal: Goal) {
+        bind(to: navigation, with: goal)
+    }
+    
+    func bind(to navigation: GoalNavigation, with goal: Goal) {
+
+        certPhotoDataInput
+            .bind(to: previewPhotoData)
+            .disposed(by: disposeBag)
+        
+        nextButtonTouched
+            .withLatestFrom(previewPhotoData)
+            .map { Photo(categoryTitle: goal.category, goalTitle: goal.title, imageData: $0, date: Date(), color: "") }
+            .map { BehaviorRelay(value: $0) }
+            .bind(to: navigation.showPhotoEditView)
+            .disposed(by: disposeBag)
+    }
+}
