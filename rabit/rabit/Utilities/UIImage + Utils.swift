3ea331b51ae32a5ddb8cb8ded631e983d1d2165e
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-10-18 14:34:52 +0900

    fix: resized(to:) 제거된 부분 다시 추가

--- rabit/rabit/Utilities/UIImage + Utils.swift
+++ rabit/rabit/Utilities/UIImage + Utils.swift
@@ -1,6 +1,13 @@
 import UIKit
 
 extension UIImage {
+    
+    func resized(to size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+    
     static func imageWithColor(color: UIColor?, size: CGSize) -> UIImage {
         let rect: CGRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
@@ -14,11 +21,11 @@ extension UIImage {
     }
     
     func cropImageToSquare() -> UIImage {
-
+        
         let imageWidth = self.size.width
         let imageHeight = self.size.height
         let squareLength = min(imageWidth,imageHeight)
-
+        
         let centerRect = CGRect(
             x: (imageHeight - squareLength) * 0.5,
             y: 0,
@@ -28,35 +35,36 @@ extension UIImage {
         
         guard let croppedImage = self.cgImage?.cropping(to: centerRect) else { return self }
         return UIImage(cgImage: croppedImage, scale: 1.0, orientation: self.imageOrientation)
-
-    func overlayText(of photo: Photo) -> UIImage? {
-        let text = DateConverter.convertToDateString(date: photo.date)
-        let textColor = UIColor(hexRGB: photo.color) ?? .white
-        let fontSize: CGFloat = 100
-        let imageSize = size
-        let textFont = UIFont(name: photo.style.name, size: fontSize) ?? UIFont.systemFont(ofSize: 100)
-
-        let attributedString = NSMutableAttributedString(string: text)
-        attributedString.addAttributes(
-            [NSAttributedString.Key.font: textFont,
-             NSAttributedString.Key.foregroundColor: textColor],
-            range: (text as NSString).range(of: text)
-        )
-
-        let image = UIImage(data: photo.imageData) ?? UIImage()
-
-        UIGraphicsBeginImageContextWithOptions(imageSize, false, 1.0)
-
-        image.draw(in: CGRect(origin: .zero, size: imageSize))
         
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        label.attributedText = attributedString
-        label.drawText(in: CGRect(origin: .zero, size: imageSize))
-
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
+        func overlayText(of photo: Photo) -> UIImage? {
+            let text = DateConverter.convertToDateString(date: photo.date)
+            let textColor = UIColor(hexRGB: photo.color) ?? .white
+            let fontSize: CGFloat = 100
+            let imageSize = size
+            let textFont = UIFont(name: photo.style.name, size: fontSize) ?? UIFont.systemFont(ofSize: 100)
+            
+            let attributedString = NSMutableAttributedString(string: text)
+            attributedString.addAttributes(
+                [NSAttributedString.Key.font: textFont,
+                 NSAttributedString.Key.foregroundColor: textColor],
+                range: (text as NSString).range(of: text)
+            )
+            
+            let image = UIImage(data: photo.imageData) ?? UIImage()
+            
+            UIGraphicsBeginImageContextWithOptions(imageSize, false, 1.0)
+            
+            image.draw(in: CGRect(origin: .zero, size: imageSize))
+            
+            let label = UILabel()
+            label.numberOfLines = 0
+            label.textAlignment = .center
+            label.attributedText = attributedString
+            label.drawText(in: CGRect(origin: .zero, size: imageSize))
+            
+            let newImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return newImage
+        }
     }
 }
