33938c41b8e5d73f6b22fd6ba66aca8463e21612
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-10-18 13:19:39 +0900

    fix: Photo 누락된 속성 추가

--- rabit/rabit/Goal/CertPhotoCamera/CertPhotoCameraViewModel.swift
+++ rabit/rabit/Goal/CertPhotoCamera/CertPhotoCameraViewModel.swift
@@ -32,7 +32,7 @@ final class CertPhotoCameraViewModel: CertPhotoCameraViewInput, CertPhotoCameraV
         
         nextButtonTouched
             .withLatestFrom(previewPhotoData)
-            .map { Photo(categoryTitle: goal.category, goalTitle: goal.title, imageData: $0, date: Date(), color: "") }
+            .map { Photo(categoryTitle: goal.category, goalTitle: goal.title, imageData: $0, date: Date(), color: "", style: .none) }
             .map { BehaviorRelay(value: $0) }
             .bind(to: navigation.showPhotoEditView)
             .disposed(by: disposeBag)
