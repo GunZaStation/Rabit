e4b8151375f2dfaabc5952168a2e8c9195132b2c
Sol <hskim6521@naver.com>
2022-12-15 16:12:12 +0900

    refactor: Goal 생성자에서 사용되는 PropertyWrapper 수정
    - 첫 날의 알림 시간이 지난 후에 생성된 Goal의 target에서 1을 빼주는 로직 추가

--- rabit/rabit/Goal/Models/Goal.swift
+++ rabit/rabit/Goal/Models/Goal.swift
@@ -9,20 +9,32 @@ struct DayCountable {
         return dayCount
     }
     
-    init(period: Period, days: Set<Day>, createdDate: Date) {
-        self.dayCount = countAllDays(period: period, days: days, createdDate: createdDate)
+    init(period: Period, certTime: CertifiableTime, createdDate: Date) {
+        self.dayCount = countAllDays(period: period, certTime: certTime, createdDate: createdDate)
     }
     
-    private func countAllDays(period: Period, days: Set<Day>, createdDate: Date) -> Int {
+    private func countAllDays(period: Period, certTime: CertifiableTime, createdDate: Date) -> Int {
+        let days = certTime.days.selectedValues
         var count = 0
         for date in stride(from: period.start, through: period.end, by: 60*60*24) {
             guard let day = Day(
                 rawValue: Calendar.current.component(.weekday, from: date) - 1
-            ), date >= createdDate else {
+            )  else {
                 continue
             }
             if days.contains(day) { count += 1 }
         }
+        
+        let alarmEndTimeInSeconds = certTime.end.hour * 60 * 60 + certTime.end.minute * 60 + certTime.end.seconds
+        let firstAlarmEndTimeInDate = Date(
+            timeInterval: TimeInterval(alarmEndTimeInSeconds),
+            since: period.start
+        )
+        
+        if createdDate > firstAlarmEndTimeInDate  {
+            count -= 1
+        }
+        
         return count
     }
 }
@@ -58,7 +70,7 @@ struct Goal: Equatable {
         self.category = category
         self.createdDate = createdDate
         
-        @DayCountable(period: period, days: certTime.days.selectedValues, createdDate: createdDate)
+        @DayCountable(period: period, certTime: certTime, createdDate: createdDate)
         var target
         
         self.target = target
